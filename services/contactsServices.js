1a86771864cf66aa58a87df1e1ad607b2df4e700
Maksimka66 <maxfese@gmail.com>
2024-05-14 21:07:40 +0300

    Update contactsServices.js

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,6 +1,1 @@
-import * as fs from "node:fs/promises";
-import run from "../db/db.js";
-import crypto from "node:crypto";
-import "dotenv/config";
 
-export async function updateStatusContact(contactId, body) {}

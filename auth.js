585c390fc4cedb1bc01dd9acb85bec7d03601ee7
Maksimka66 <maxfese@gmail.com>
2024-05-26 19:19:48 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,24 +6,24 @@ function authCheck(req, res, next) {
   const authorizationHeader = req.headers.authorization;
 
   if (!authorizationHeader) {
-    throw HttpError(401);
+    return next(HttpError(401));
   }
 
   const [bearer, token] = authorizationHeader.split(" ", 2);
 
   if (bearer !== "Bearer") {
-    throw HttpError(401);
+    return next(HttpError(401));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
     if (err) {
-      throw HttpError(401);
+      return next(HttpError(401));
     }
     try {
       const definiteUser = await User.findById(decode.id);
 
       if (definiteUser === null || definiteUser.token !== token) {
-        throw HttpError(401);
+        return next(HttpError(401));
       }
 
       req.user = definiteUser;

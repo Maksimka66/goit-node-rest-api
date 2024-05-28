a1d85ba61481bf1386e07585eaa6c1bc6a9610aa
Maksimka66 <maxfese@gmail.com>
2024-05-28 23:07:16 +0300

    excess is removed

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -21,17 +21,3 @@ export const validateBody = (schema) => {
 
   return func;
 };
-
-export const validateToken = (schema) => {
-  const func = (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res
-        .status(400)
-        .json({ message: "Помилка від Joi або іншої бібліотеки валідації" });
-    }
-    next();
-  };
-
-  return func;
-};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,14 +5,14 @@ import {
   logoutUser,
   getUserByToken,
 } from "../controllers/usersControllers.js";
-import { validateToken } from "../helpers/validateBody.js";
+import { validateBody } from "../helpers/validateBody.js";
 import { loginSchema, registerSchema } from "../schemas/usersSchemas.js";
 import authCheck from "../auth.js";
 
 const usersRouter = express.Router();
 
-usersRouter.post("/register", validateToken(registerSchema), registerUser);
-usersRouter.post("/login", validateToken(loginSchema), loginUser);
+usersRouter.post("/register", validateBody(registerSchema), registerUser);
+usersRouter.post("/login", validateBody(loginSchema), loginUser);
 usersRouter.post("/logout", authCheck, logoutUser);
 usersRouter.get("/current", authCheck, getUserByToken);
 

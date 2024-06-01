c6f64b911c70aae2656c572b753d0a16f80975c1
Maksimka66 <maxfese@gmail.com>
2024-06-01 21:11:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.resolve("public")));
+app.use("/avatars", express.static(path.resolve("public/avatars")));
 
 app.use("/users", usersRouter);
 app.use("/api/contacts", authCheck, contactsRouter);
